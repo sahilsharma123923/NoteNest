7fc9e4dff5b87a2d8d55517ec380b4ea70632ab4
Sahil Sharma <sahilsharma901590@gmail.com>
2026-07-11 23:35:25 +0530

    Last Commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const connectDB=require('./src/config/db')
 
 connectDB();
 
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
--- src/app.js
+++ src/app.js
@@ -3,10 +3,17 @@ const express=require('express');
 const cookieParser=require('cookie-parser')
 const authRouter=require('./routes/auth.route')
 const noteRouter=require('./routes/notes.route')
+const cors = require("cors");
 
 const app=express();
 app.use(express.json())
 app.use(cookieParser());
+
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use("/api/auth",authRouter);
 app.use("/api/notes",noteRouter);
